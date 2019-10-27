1756334c63594d3e85f11840c37b2eacc6ba32ba
Farrah Carter <carterfarraha@gmail.com>
2019-10-27 18:38:45 -0400

    added starting and stopping gif on click

--- assets/script.js
+++ assets/script.js
@@ -28,17 +28,32 @@ function add(newAnimal) {
 }
 
 function getGif() {
+    //takes animal name from input entered
     let animalName = $(this).attr("data-name");
-    let queryURL = "https://api.giphy.com/v1/gifs/search?api_key=CbRv29mIUSwkTAVauYUvcQ8lOGyxCop2&q=" + animalName + "&limit=10&offset=0&rating=G&lang=en"
+
+    //object containing parameters 
+    const queryParams = {
+        "api_key": "CbRv29mIUSwkTAVauYUvcQ8lOGyxCop2",
+        q: animalName,
+        "limit": 10,
+        "offset": 0,
+        "rating": "G",
+        "lang": "en"
+    };
+    //call parameters from object
+    let paramString = $.param(queryParams);
+    let queryURL = "https://api.giphy.com/v1/gifs/search?" + paramString;
+    //queryURL = "https://api.giphy.com/v1/gifs/search?api_key=CbRv29mIUSwkTAVauYUvcQ8lOGyxCop2&q=" + animalName + "&limit=10&offset=0&rating=G&lang=en"
 
     $.ajax({
+        //calls giphy search
         url: queryURL,
         Method: "GET"
     }).then(function (response) {
         console.log(response);
-
+        //creates image div for each item in response
         for (let i = 0; i < response.data.length; i++) {
-            const gifContent = `<div>
+            const gifContent = `<div class="col-2.4">
             ${"<img src=" + response.data[i].images.fixed_width_still.url + "/>"}</div>`
             $("#gifRow").prepend(gifContent);
 
@@ -48,6 +63,21 @@ function getGif() {
     })
 }
 
+function stopStartGif() {
+    //grabs image source attribute
+    let imageURL = $(this).attr("src");
+    //amends image url to either still or active version
+    if (imageURL.includes("200w_s")) {
+        imageURL = imageURL.replace("200w_s", "200w");
+    }
+    else {
+        imageURL = imageURL.replace("200w", "200w_s")
+    }
+    //changes attribute in the DOM
+    $(this).attr("src", imageURL)
+
+
+}
 //waits for page to load before running function
 $(document).ready(function () {
     //renders inital set of animals
@@ -72,5 +102,8 @@ $(document).ready(function () {
 
     //searches for gifs on user click
     $(document).on("click", ".animalBtn", getGif);
+
+    //runs stops and starts gif on user click
+    $(document).on("click", "img", stopStartGif)
 
 });
